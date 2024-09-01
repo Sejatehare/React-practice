0f916bc43426d93b4e554080869349ba1d92cff5
Sejatehare <sejaltehare3@gmail.com>
2024-09-01 20:58:28 +0530

    Form added

--- restaurant_app/src/components/cart/cartIcon.js
+++ restaurant_app/src/components/cart/cartIcon.js
@@ -1,4 +1,4 @@
-const cartIcon = () => {
+const CartIcon = () => {
     return (
       <svg
         xmlns='http://www.w3.org/2000/svg'
@@ -10,4 +10,4 @@ const cartIcon = () => {
     );
   };
   
-  export default cartIcon;
+  export default CartIcon;
--- restaurant_app/src/components/layout/HeaderCartButton.js
+++ restaurant_app/src/components/layout/HeaderCartButton.js
@@ -1,9 +1,9 @@
-import cartIcon from "../cart/cartIcon";
+import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = () => {
     return <button>
-        <span className={classes.button}><cartIcon></cartIcon></span>
+        <span className={classes.button}><CartIcon></CartIcon></span>
         <span className={classes.icon}>Your Cart</span>
         <span className={classes.badge}>3</span>
     </button>
