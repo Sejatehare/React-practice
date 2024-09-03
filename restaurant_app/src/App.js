9e50e2252ce00f87cf1e5b27274a01544205e690
Sejatehare <sejaltehare3@gmail.com>
2024-09-03 17:44:59 +0530

    showing number of cart items next to carticon

--- restaurant_app/src/App.js
+++ restaurant_app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ function App() {
   }
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
