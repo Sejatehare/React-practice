5e204b87663f8be4856af6f74350126896c74ab2
Sejatehare <sejaltehare3@gmail.com>
2025-07-16 13:32:54 +0530

    task 5 home page added

--- e-commerce-project/src/Components/Routers/Routers.js
+++ e-commerce-project/src/Components/Routers/Routers.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import ProductList from "../Products/ProductList";
 import About from "../Pages/About";
+import Home from "../Pages/Home";
 
 const Routers = () => {
     return <>
         <Routes>
-            <Route path="/" element={<ProductList/>}></Route>
+            <Route path="/" element={<Home/>}></Route>
             <Route path="/store" element={<ProductList/>}></Route>
             <Route path="/about" element={<About/>}></Route>
         </Routes>
