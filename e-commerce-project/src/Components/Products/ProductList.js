0e8bf29499654ce8b1de8f24c3fdbddd9bebc842
Sejatehare <sejaltehare3@gmail.com>
2025-07-23 20:15:19 +0530

    task 13 product page implemented

--- e-commerce-project/src/Components/Products/ProductList.js
+++ e-commerce-project/src/Components/Products/ProductList.js
@@ -1,6 +1,8 @@
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import CartContext from "../Context/CartContext";
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
+import { routePath } from "../Routers/Routers";
 
 const ProductList = () => {
     const productsArr = [
@@ -38,6 +40,11 @@ const ProductList = () => {
     ];
 
     const cartCtx = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const productClickHandler = () => {
+        navigate(routePath.ProductPage);
+    };
 
     const addItemToCart = (item) => {
         cartCtx.addItem(item);
@@ -56,7 +63,7 @@ const ProductList = () => {
                                     {product.title}
                                 </Card.Title>
                                 <Card.Body>
-                                    <Card.Img variant="top" src={product.imageUrl} />
+                                    <Card.Img onClick={productClickHandler} variant="top" src={product.imageUrl} />
                                     <Card.Text className="pt-3">${product.price}</Card.Text>
                                     <Button variant="primary" onClick={() => addItemToCart(product)}>Add to Cart</Button>
                                 </Card.Body>
--- e-commerce-project/src/Components/Routers/Routers.js
+++ e-commerce-project/src/Components/Routers/Routers.js
@@ -3,14 +3,25 @@ import ProductList from "../Products/ProductList";
 import About from "../Pages/About";
 import Home from "../Pages/Home";
 import ContactUs from "../Pages/ContactUs";
+import ProductPage from "../Products/ProductPage";
+
+export const routePath = {
+    Home: "/",
+    Store: "/store",
+    About: "/about",
+    ContactUs: "/contact-us",
+    ProductPage: "/store/product",
+};
 
 const Routers = () => {
+
     return <>
         <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/store" element={<ProductList/>}></Route>
-            <Route path="/about" element={<About/>}></Route>
-            <Route path="/contact-us" element={<ContactUs/>}></Route>
+            <Route path={routePath.Home} element={<Home />} />
+            <Route path={routePath.Store} element={<ProductList />} />
+            <Route path={routePath.ProductPage} element={<ProductPage />} />
+            <Route path={routePath.About} element={<About />} />
+            <Route path={routePath.ContactUs} element={<ContactUs />} />
         </Routes>
     </>
 }
