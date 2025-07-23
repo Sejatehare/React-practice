02f1cc9c9eccf889a06a979acd294310b0e0672c
Sejatehare <sejaltehare3@gmail.com>
2025-07-23 21:02:29 +0530

    task 23 protecting product

--- e-commerce-project/src/Components/Routers/Routers.js
+++ e-commerce-project/src/Components/Routers/Routers.js
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import ProductList from "../Products/ProductList";
 import About from "../Pages/About";
 import Home from "../Pages/Home";
 import ContactUs from "../Pages/ContactUs";
 import ProductPage from "../Products/ProductPage";
 import AuthForm from "../Auth/AuthForm";
+import { useContext } from "react";
+import AuthContext from "../Context/AuthContext";
 
 export const routePath = {
     Home: "/",
@@ -17,10 +19,19 @@ export const routePath = {
 
 const Routers = () => {
 
+    const authCtx = useContext(AuthContext)
+
     return <>
         <Routes>
             <Route path={routePath.Home} element={<Home />} />
-            <Route path={routePath.Store} element={<ProductList />} />
+            <Route path={routePath.Default} element={<Home />} />
+            <Route path={routePath.Store} element={
+                authCtx.isLoggedIn ? (
+                <ProductList />
+                ) : (
+                <Navigate to={routePath.Login} />
+                )
+            }/>
             <Route path={routePath.ProductPage} element={<ProductPage />} />
             <Route path={routePath.About} element={<About />} />
             <Route path={routePath.Login} element={<AuthForm/>}/>
