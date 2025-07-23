9291e13fc2761661c3f441a01deef579fa6d62a9
Sejatehare <sejaltehare3@gmail.com>
2025-07-23 19:54:10 +0530

    task 14 page with react router

--- e-commerce-project/src/Components/Routers/Routers.js
+++ e-commerce-project/src/Components/Routers/Routers.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import ProductList from "../Products/ProductList";
 import About from "../Pages/About";
 import Home from "../Pages/Home";
+import ContactUs from "../Pages/ContactUs";
 
 const Routers = () => {
     return <>
@@ -9,6 +10,7 @@ const Routers = () => {
             <Route path="/" element={<Home/>}></Route>
             <Route path="/store" element={<ProductList/>}></Route>
             <Route path="/about" element={<About/>}></Route>
+            <Route path="/contact-us" element={<ContactUs/>}></Route>
         </Routes>
     </>
 }
